62b3f87e2a56db3d0913fc398547561df9a1a79c
Jyotisaini123 <lalitasaini9289@gmail.com>
2024-05-11 13:24:23 +0530

    add contact section

--- scrpt.js
+++
@@ -1,9 +0,0 @@
-let careers = document.getElementById("#careers");
-
-let about = document.getElementById("#section_about-1");
-var divs = 0;
-
-careers.addEventListener("click", function () {
-  careers.style.display = "block";
-  careers.style.opacity = "1";
-});
